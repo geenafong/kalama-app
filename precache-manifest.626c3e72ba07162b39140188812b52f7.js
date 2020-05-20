b6bf4de8bc1698e680174893a8e7f990387fe787
geenafong <geenafong@gmail.com>
2020-05-19 20:03:54 -0700

    Updates

---
+++ precache-manifest.626c3e72ba07162b39140188812b52f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cc6d6654303468bbbd3c",
+    "url": "/static/js/main.cc6d6654.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "cc6d6654303468bbbd3c",
+    "url": "/static/css/main.939d8d98.chunk.css"
+  },
+  {
+    "revision": "5bcf7b62a8a042532216e644100d00d3",
+    "url": "/index.html"
+  }
+];
