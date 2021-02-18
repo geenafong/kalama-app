e01f924cb9c3fc2a8d3e69402d71fe103240ef94
geenafong <geenafong@gmail.com>
2021-02-17 17:56:44 -0800

    add untracked files

---
+++ src/components/contactUs.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="contactUs">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">Contact Us</span>
+                    <span className="email"><i className="icon-mail"></i> <a href="mailto:kalama-river-retreat@google.com">kalama-river-retreat@google.com</a></span>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            </div>
+        </div>
+        </section>
+      </div>
+        
+
+    )
+    }
+    }
